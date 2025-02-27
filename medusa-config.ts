3ff4f84a342e7e5e9db8194366f07012ad6a341d
Tee Woei Jer <7721899+teewoeijer@users.noreply.github.com>
2025-02-27 13:27:20 +0800

    Update medusa-config.ts

--- medusa-config.ts
+++ medusa-config.ts
@@ -11,6 +11,17 @@ module.exports = defineConfig({
       authCors: process.env.AUTH_CORS!,
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
-    }
-  }
+    },
+    redisUrl: process.env.REDIS_URL,
+  },
+  plugins: [
+    {
+      resolve: `medusa-events-webhooks`,
+      options: {
+        enableUI: true,
+        TOKEN_SECRET: 'JrKSQgmXc7FUg7FyWeTK',
+        MAX_RETRY_COUNT: 5,
+      },
+    },
+  ],
 })
